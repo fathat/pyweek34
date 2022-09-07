200d29e33dd314cec55204a81d6602f42a220292
airbrett <mixmasterbrett@gmail.com>
2022-09-06 22:13:54 -0600

    give humanoids a body to move and animate

--- humanoid.py
+++ humanoid.py
@@ -2,14 +2,25 @@ import pymunk
 import math
 import utils
 
+from direct.actor.Actor import Actor
+from enum import Enum, auto
+
+class State(Enum):
+    IDLE = auto()
+    RUN_L = auto()
+    RUN_R = auto()
+    WAVE = auto()
+
 class humanoid:
     def __init__(self, base, space):
         self.target = None
         self.space = space
         self.destroyed = False
 
-        self.bodyNode = base.loader.loadModel("models/Capsule.stl")
+        self.state = State.IDLE
+        self.bodyNode = Actor("art/creative-commons/Ultimate Modular Men- Feb 2022/Individual Characters/glTF/Spacesuit.gltf")
         self.bodyNode.reparentTo(base.render)
+        self.bodyNode.loop("Idle")
 
         poly = self.makePoly(0.36, 1.74, 10)
 
@@ -29,15 +40,16 @@ class humanoid:
         self.bodyNode.remove_node()
 
     def update(self,dt):
+        self.new_state = State.IDLE
         self.body.angle = 0
         self.pos = self.body.position
 
         self.bodyNode.setPos(self.pos.x,0,self.pos.y)
-        self.bodyNode.setHpr(0,0,-utils.radians_to_degrees(self.body.angle))
+        self.bodyNode.setP(0)
 
         if self.target:
             filter = pymunk.ShapeFilter(mask=utils.CATEGORY_PLAYER)
-            result = self.space.point_query_nearest(self.pos, 25, filter)
+            result = self.space.point_query_nearest(self.pos, 20, filter)
             
             if result != None:
                 diff = result.point - self.pos
@@ -45,6 +57,29 @@ class humanoid:
                 if diff.y < 5:
                     force = 100 * diff.normalized()
                     self.body.apply_force_at_local_point((force.x, 0), (0, 0))
+                    
+                    if force.x > 0:
+                        self.new_state = State.RUN_L
+                    else:
+                        self.new_state = State.RUN_R
+
+            if self.new_state == State.IDLE:
+                if self.target.pos.get_distance(self.pos) < 30:
+                    self.new_state = State.WAVE
+
+        if self.new_state != self.state:
+            self.state = self.new_state
+            if self.state == State.RUN_L:
+                self.bodyNode.setH(90)
+                self.bodyNode.loop("Run")
+            elif self.state == State.RUN_R:
+                self.bodyNode.setH(-90)
+                self.bodyNode.loop("Run")
+            elif self.state == State.WAVE:
+                self.bodyNode.loop("Wave")
+                self.bodyNode.setH(0)
+            else:
+                self.bodyNode.loop("Idle")
 
     def makePoly(self, body_w, body_h, subdivisions):
         body_radius = body_w/2
